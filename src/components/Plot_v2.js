7a177766c3f8dcdda733a36c8c0d23e60ce6c3a0
Dannika <71364766+dannikate@users.noreply.github.com>
2025-01-28 14:59:21 +1000

    address plot issues; see dev docs notes for further details

--- src/components/Plot_v2.js
+++ src/components/Plot_v2.js
@@ -7,7 +7,7 @@ let dataValues = [];
 
 // Initialize traces with different fill options
 let traces = {
-    GH: createTrace('GH 40:1', [], [], 'rgba(234,234,0,0.97)', 'rgba(234,234,0,0.97)', 'lines', 'none', { dash: 'longdash' }),  // No fill
+    GH: createTrace('GH 40:1', [], [], 'rgba(255,238,0,0.97)', 'rgba(234,234,0,0.97)', 'lines', 'none', { dash: 'longdash' }),  // No fill
     WL: createTrace('Freshwater [0 - 250] mg/L Cl-', [], [], 'rgb(68, 114, 196)', 'rgba(68, 114, 196, 0.2)', 'lines','tonexty'),    // Fill to next y-axis value
     BoFL: createTrace('Brackish (250 - 81,000] mg/L Cl-', [], [], 'rgb(66, 255, 66)', 'rgba(204, 255, 204, 0.6)', 'lines', 'tonexty'), // Fill to next x-axis value
     BoBL: createTrace('Saline (81,000 - 15,000] mg/L Cl-', [], [], 'rgb(0, 155, 78)', 'rgba(0, 204, 102, 0.6)', 'lines', 'tonexty'),       // No fill
@@ -77,7 +77,7 @@ function checkLatestValue(y_vals) {
             break;
         }
     }
-    return pos;
+    return pos + 1;
 }
 
 /*
