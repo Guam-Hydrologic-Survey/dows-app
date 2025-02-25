14aca2941ec3f06efeea08ea3f238d6ecd93e4b4
Dannika <71364766+dannikate@users.noreply.github.com>
2025-02-25 11:52:55 +1000

    update links, buttons

--- src/components/About_v0.js
+++ src/components/About_v0.js
@@ -25,17 +25,16 @@ export function About(element) {
             </p>
           </div>
           <div class="modal-footer about-btns">
-            <a class="btn btn-primary" href="#" target="_blank" rel="noreferrer noopener" role="button" title="Coming soon">WERI Technical Report <b>(coming soon)</b></a>
-            <!-- Dropdown for links to GHS maps libraries -->
+            <a class="btn btn-primary" title="Coming soon!" href="#" target="_blank" rel="noreferrer noopener" role="button">WERI Technical Report</a>
             <div class="dropdown">
-              <button class="btn btn-success dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" title="Click to expand list">
-                Map Series
-              </button>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="https://guamhydrologicsurvey.uog.edu/index.php/mappfx/" title="GHS MAppFx Library">MAppFx Library</a></li>
-                <li><a class="dropdown-item" href="https://guamhydrologicsurvey.uog.edu/index.php/interagency-maps/" title="GHS Interagency Maps">Interagency Maps</a></li>
-              </ul>
-            </div>
+                <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  WERI Map Series
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="https://guamhydrologicsurvey.uog.edu/index.php/mappfx/" title="MAppFx Library on GHS" target="_blank" rel="noreferrer noopener">MAppFx Library</a></li>
+                  <li><a class="dropdown-item" href="https://guamhydrologicsurvey.uog.edu/index.php/web-mapps/" title="Web MApps on GHS" target="_blank" rel="noreferrer noopener">Web MApps</a></li>
+                </ul>
+              </div>
           </div>
         </div>
       </div>
