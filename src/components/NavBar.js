939d9e701355afc3d26dae8bca38109fc561080d
Dannika <71364766+dannikate@users.noreply.github.com>
2025-02-25 11:54:17 +1000

    update navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ Return: none
 export function NavBar(element) {
 
   // update this with link to designated GHS page/post, if available 
-  const ghsPageLink = "https://guamhydrologicsurvey.uog.edu/index.php/mappfx/";
+ //  const ghsPageLink = "https://guamhydrologicsurvey.uog.edu/index.php/mappfx/";
 
   element.innerHTML = /*html*/
   `
@@ -21,13 +21,6 @@ export function NavBar(element) {
           <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
         </svg>
       </button>
-
-      <!-- link to GHS page -->
-      <a class="btn btn-primary" href="${ghsPageLink}" target="_blank" rel="noreferrer noopener" role="button" title="Home">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
-          <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
-        </svg>
-      </a>
     
       <!-- dropdown for links to WERI and GHS sites -->
       <div class="btn-group" role="group">
