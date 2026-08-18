db3b8ed96dcc865a85a4dfab40d0ac34724b0173
Dannika <71364766+dannikate@users.noreply.github.com>
2026-08-18 14:41:00 +1000

    remove pointSelectBtn from leaflet controls (bottom right)
    just keep lasso selection as primary selection tool for viewing multiple profiles in a single window

--- src/components/LMap.js
+++ src/components/LMap.js
@@ -162,11 +162,11 @@ export function LMap(element) {
 
     // console.log(pointSelectBtn.options.states);
 
-    const pointSelectionControls = L.easyBar([
-        pointSelectBtn,
-    ], { position: "bottomright" });
+    // const pointSelectionControls = L.easyBar([
+    //     pointSelectBtn,
+    // ], { position: "bottomright" });
 
-    pointSelectionControls.addTo(map);
+    // pointSelectionControls.addTo(map);
     
     lassoControl.addTo(map); 
 
